d0fd314a7f541d2ab02c771feb07eb3534be486f
Rathijit Paul <alienpapon009@gmail.com>
2021-06-17 18:50:46 +0600

    :bug: Fix Country Schema Fields Defination :construction: Add Country CRUD APIs

--- api/backend/src/controllers/explore/country.js
+++ api/backend/src/controllers/explore/country.js
@@ -1,7 +1,138 @@
 const { Country } = require("../../models/explore");
 
+const checkValidBody = (body, fields) => {
+    const isValid = fields.every((field) => {
+        return field in body;
+    });
+
+    return isValid;
+}
+
+const convertValidBody = (body, fields) => {
+    const validBody = {}
+
+    for (const field of fields) {
+        validBody[field] = body[field];
+    }
+
+    return validBody;
+}
+
+const createCountry = async (req, res) => {
+    const fields = ["name", "description", "banner"];
+
+    try {
+        const isValid = checkValidBody(req.body, fields);
+        if (!isValid) {
+            throw new Error("Invalid Fields");
+        }
+        const body = convertValidBody(req.body, fields);
+
+        const country = new Country(body);
+        await country.save();
+
+        res.status(200).send(country);
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+        });
+    }
+}
+
+const updateCountry = async (req, res) => {
+    const fields = ["name", "description", "banner"];
+
+    try {
+        const isValid = checkValidBody(req.body, fields);
+        if (!isValid) {
+            throw new Error("Invalid Fields");
+        }
+        const body = convertValidBody(req.body, fields);
+
+        const country = await Country.findOne({
+            name: body.name
+        });
+
+        for (field of fields) {
+            country[field] = body[field];
+        }
+        await country.save();
+
+        res.status(200).send(country);
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+        });
+    }
+}
+
+const getCountryByName = async (req, res) => {
+    const fields = ["name"];
+
+    try {
+        const isValid = checkValidBody(req.query, fields);
+        if (!isValid) {
+            throw new Error("Invalid Fields");
+        }
+        const body = convertValidBody(req.query, fields);
+
+        const country = await Country.findOne({
+            name: body.name
+        });
+        if (!country) {
+            throw new Error("Country Not Found");
+        }
+
+        res.status(200).send(country);
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+        });
+    }
+}
+
+const getAllCountry = async (req, res) => {
+    try {
+        const countries = await Country.find();
+
+        res.status(200).send(countries);
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+        });
+    }
+}
+
+const deleteCountry = async (req, res) => {
+    const fields = ["name"];
+
+    try {
+        const isValid = checkValidBody(req.query, fields);
+        if (!isValid) {
+            throw new Error("Invalid Fields");
+        }
+        const body = convertValidBody(req.query, fields);
+
+        await Country.findOneAndDelete({
+            name: body.name
+        });
+
+        res.status(200).send({
+            message: "successfully deleted",
+        });
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+        });
+    }
+}
+
 const countryController = {
-    
+    createCountry,
+    updateCountry,
+    getCountryByName,
+    getAllCountry,
+    deleteCountry,
 };
 
 module.exports = countryController;
--- api/backend/src/routers/explore/country.js
+++ api/backend/src/routers/explore/country.js
@@ -5,4 +5,14 @@ const { countryController } = require("../../controllers/explore");
 
 const countryRouter = express.Router();
 
+countryRouter.post('/create', adminAuth, countryController.createCountry);
+
+countryRouter.post('/update', adminAuth, countryController.updateCountry);
+
+countryRouter.delete('/remove', adminAuth, countryController.deleteCountry);
+
+countryRouter.get('/', countryController.getCountryByName);
+
+countryRouter.get('/all', countryController.getAllCountry);
+
 module.exports = countryRouter;
