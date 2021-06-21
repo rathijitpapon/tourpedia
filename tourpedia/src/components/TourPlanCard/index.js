ca044aaf600ea04925cacb4266797383f94ca2c6
Rathijit Paul <alienpapon009@gmail.com>
2021-06-22 00:50:26 +0600

    :construction: Tour Plan Small Card Added

---
+++ tourpedia/src/components/TourPlanCard/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import {Image} from 'react-bootstrap';
+
+import "./styles.css";
+
+const TourPlanCard = (props) => {
+    const tourplan = props.tourplan;
+
+    return ( 
+        <Link
+            to={"/tourplan/" + tourplan.name + "-" + tourplan.id}
+            className="tour-plan-card-main"
+        >
+            <Image
+                className="tour-plan-card-image"
+                src={tourplan.banner}
+                alt="tourplan img"
+            />
+            <div className="tour-plan-card-middle">
+                <div className="tour-plan-card-title">
+                    {tourplan.name}
+                </div>
+                <div className="tour-plan-card-description">
+                    {tourplan.description}
+                </div>
+            </div>
+        </Link>
+     );
+}
+ 
+export default TourPlanCard;
