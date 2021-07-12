0181cd205ae95a65944eb115b5c2b862d9daf58b
Rathijit Paul <alienpapon009@gmail.com>
2021-07-12 19:19:22 +0600

    :sparkles: Explore Service Integrated

---
+++ travelagency/src/services/exploreService.js
@@ -0,0 +1,60 @@
+import httpService from "./httpService";
+
+import config from "../config/config.json";
+
+const baseURL = config.apiBaseURL + "/explore";
+
+const getExploreByName = (name, explore) => {
+    const url = `${baseURL}/${explore}?name=${name}`;
+
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No Explore Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getAllExplore = (explore) => {
+    const url = `${baseURL}/${explore}/all`;
+
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No Explore Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const userAuthService = {
+    getExploreByName,
+    getAllExplore,
+};
+
+export default userAuthService;
