3899f10dd629741aa3c00672071dd753d96da0a0
Rathijit Paul <alienpapon009@gmail.com>
2021-07-24 21:21:31 +0600

    :bug: Event Details Page Removed From Travel Agency Frontend

--- travelagency/src/layouts/index.js
+++ travelagency/src/layouts/index.js
@@ -4,7 +4,6 @@ import {Switch, Route, Redirect} from "react-router-dom";
 import EditEvent from "../views/EditEvent";
 import Enter from "../views/Enter";
 import Event from "../views/Event";
-import EventDetails from "../views/EventDetails";
 import Guide from "../views/Guide";
 import Home from "../views/Home";
 import Profile from "../views/Profile";
@@ -16,7 +15,6 @@ const MainLayout = () => {
         <Route exact path="/profile" component={Profile} ></Route>
         <Route exact path="/home" component={Home} ></Route>
         <Route exact path="/event" component={Event} ></Route>
-        <Route exact path="/event/:eventName" component={EventDetails} ></Route>
         <Route exact path="/event/edit/:eventId" component={EditEvent} ></Route>
         <Route exact path="/guide" component={Guide} ></Route>
         <Route exact path="/" component={Enter} ></Route>
