4bba7471afc2073cc811d0b2a556028eb96eece1
Rathijit Paul <alienpapon009@gmail.com>
2021-07-25 00:52:26 +0600

    :sparkles: Tour Plan API Service Added

---
+++ admin/src/services/tourplanService.js
@@ -0,0 +1,139 @@
+import httpService from "./httpService";
+import authService from "./authService";
+
+import config from "../config/config.json";
+
+const baseURL = config.apiBaseURL + "/pedia/tourplan";
+
+const createTourPlan = (tourPlan) => {
+    const url = `${baseURL}/create`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.post(url, tourPlan).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Unauthorized",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const editTourPlan = (tourPlan, tourPlanId) => {
+    const url = `${baseURL}/update?id=${tourPlanId}`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.post(url, tourPlan).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Unauthorized",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const deleteTourPlan = (tourPlanId) => {
+    const url = `${baseURL}/remove?id=${tourPlanId}`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.delete(url, {}).then(res => {
+        return {
+            status: res.status,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Unauthorized",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getManyTourPlans = () => {
+    const url = `${baseURL}/all`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No TourPlan Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getTourPlanById = (id) => {
+    const url = `${baseURL}/${id}`;
+    
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "TourPlan Not Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const tourPlanService = {
+    createTourPlan,
+    editTourPlan,
+    deleteTourPlan,
+    getManyTourPlans,
+    getTourPlanById,
+};
+
+export default tourPlanService;
