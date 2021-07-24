587e62d8d6d620a09dead90e2212a8e3e91bae67
Rathijit Paul <alienpapon009@gmail.com>
2021-07-24 21:21:52 +0600

    :sparkles: Event Service Added

---
+++ travelagency/src/services/eventService.js
@@ -0,0 +1,165 @@
+import httpService from "./httpService";
+import authService from "./authService";
+
+import config from "../config/config.json";
+
+const baseURL = config.apiBaseURL + "/event/event";
+
+const createEvent = (event) => {
+    const url = `${baseURL}/create`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.post(url, event).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Unauthorized",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const editEvent = (event, eventId) => {
+    const url = `${baseURL}/update?id=${eventId}`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.post(url, event).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Unauthorized",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const deleteEvent = (eventId) => {
+    const url = `${baseURL}/remove?id=${eventId}`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.delete(url, {}).then(res => {
+        return {
+            status: res.status,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Unauthorized",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getGuidedEvents = () => {
+    const url = `${baseURL}/guidedevent`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No Event Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getManyEvents = () => {
+    const url = `${baseURL}/myevent`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No Event Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getEventById = (id) => {
+    const url = `${baseURL}/${id}`;
+    
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Event Not Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const eventService = {
+    createEvent,
+    editEvent,
+    deleteEvent,
+    getGuidedEvents,
+    getManyEvents,
+    getEventById,
+};
+
+export default eventService;
