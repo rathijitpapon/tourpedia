902d23df670d637bd2aa57490a2b8f3618cfeab9
Rathijit Paul <alienpapon009@gmail.com>
2021-07-26 13:26:25 +0600

    :sparkles: Event API Service Integrated with admin

---
+++ admin/src/services/eventService.js
@@ -0,0 +1,95 @@
+import httpService from "./httpService";
+import authService from "./authService";
+
+import config from "../config/config.json";
+
+const baseURL = config.apiBaseURL + "/event/event";
+
+const changeApproveEvent = (eventId, isApproved) => {
+    const url = `${baseURL}/approve?id=${eventId}`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.post(url, {
+        isApproved
+    }).then(res => {
+        return {
+            status: res.status,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No Event Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const changeBanEvent = (eventId, isBanned) => {
+    const url = `${baseURL}/ban?id=${eventId}`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.post(url, {
+        isBanned
+    }).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No Event Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getManyEvents = (queryMatcher) => {
+    const url = `${baseURL}/many`;
+
+    httpService.setJWT(authService.getJWT());
+    const response = httpService.get(url, {
+        params: queryMatcher
+    }).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            console.log(error.response);
+            return {
+                status: error.response.status,
+                message: "No Event Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const eventService = {
+    changeApproveEvent,
+    changeBanEvent,
+    getManyEvents,
+};
+
+export default eventService;
