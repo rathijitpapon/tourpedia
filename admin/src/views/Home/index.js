5a77185e8fa5deb3ef6be0b3e49574953ec1bcc9
Rathijit Paul <alienpapon009@gmail.com>
2021-07-26 20:25:03 +0600

    :sparkles: Admin Home Page Added

--- admin/src/views/Home/index.js
+++ admin/src/views/Home/index.js
@@ -1,10 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import {Image} from 'react-bootstrap';
 
 import LayoutWrapper from "../../layouts/LayoutWrapper";
 import "./styles.css";
 
+import userAuthService from '../../services/userAuthService';
+import Logo from '../../assets/logo.png';
+
 const Home = () => {
+    const [name, setName] = useState('');
+
     const fetchData = async () => {
+        const data = await userAuthService.getSavedAuthInfo();
+        if (data) {
+            setName(data.fullname);
+        }
     }
 
     useEffect(() => {
@@ -13,7 +23,10 @@ const Home = () => {
 
     return ( 
         <LayoutWrapper>
-            <h1 style={{textAlign: 'center'}}>Home Page</h1>
+            <Image src={Logo} alt="logo" className="home-logo"/>
+            <div className="home-title">Tour Pedia</div>
+            <div className="home-text">Administrator Platform</div>
+            <div className="home-name"><b>Current User:</b> &nbsp; {name}</div>
         </LayoutWrapper>
      );
 }
