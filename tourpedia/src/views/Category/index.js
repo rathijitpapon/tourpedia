6826baa00583797f010e923808acb95101f5766f
Rathijit Paul <alienpapon009@gmail.com>
2021-07-27 22:46:59 +0600

    :sparkles: Category Service Integrated with TourPedia

--- tourpedia/src/views/Category/index.js
+++ tourpedia/src/views/Category/index.js
@@ -1,22 +1,41 @@
 import React, {useState, useEffect} from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
+import LoadingOverlay from 'react-loading-overlay';
+import { toast } from 'react-toastify';
 
 import CategoryCard from '../../components/CategoryCard';
 import LayoutWrapper from "../../layouts/LayoutWrapper";
 import "./styles.css";
 
-import categoryData from "../../assets/dummyData/category.json";
+import exploreService from '../../services/exploreService';
 
 const Category = () => {
+
+    const [loading, setLoading] = useState(false);
+    const color = "#ffffff";
 
     const [categories, setCategories] = useState([]);
 
     const fetchData = async () => {
-        const data = [];
+        setLoading(true);
 
-        for (let i = 0; i < categoryData.length; i++) {
-            data.push(categoryData[i]);
+        let data = await exploreService.getAllExplore("category");
+        if (data.status >= 300) {
+            toast.error(data.message, {
+                position: "top-right",
+                autoClose: 4000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            setLoading(false);
+            return;
         }
+        data = data.data;
         setCategories(data);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -26,6 +45,13 @@ const Category = () => {
 
     return ( 
         <LayoutWrapper>
+            <LoadingOverlay
+                active={loading}
+                spinner={
+                    <ClipLoader color={color} loading={loading} size={50} />
+                }
+                className="loading-height"
+            >
             <div className="category-title">
                 Tour Category List
             </div>
@@ -49,7 +75,7 @@ const Category = () => {
             </div>
 
             <br />
-            
+            </LoadingOverlay>
         </LayoutWrapper>
      );
 }
