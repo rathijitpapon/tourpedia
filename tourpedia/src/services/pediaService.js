6ee63cdfe9940fcf2341c6f32a0d97529ba39f8a
Rathijit Paul <alienpapon009@gmail.com>
2021-07-28 09:24:54 +0600

    :sparkles: Pedia API Integrated

---
+++ tourpedia/src/services/pediaService.js
@@ -0,0 +1,65 @@
+import httpService from "./httpService";
+
+import config from "../config/config.json";
+
+const baseURL = config.apiBaseURL + "/pedia/pedia";
+
+const getManyPedias = () => {
+    const url = `${baseURL}/many`;
+
+    const response = httpService.get(url, {
+        params: {
+            skip: 0,
+            limit: 10000000000,
+        }
+    }).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "No Pedia Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const getPediaById = (id) => {
+    const url = `${baseURL}/${id}`;
+    
+    const response = httpService.get(url, {}).then(res => {
+        return {
+            status: res.status,
+            data: res.data,
+        };
+    }).catch(error => {
+        if(error.response && error.response.status <= 500) {
+            return {
+                status: error.response.status,
+                message: "Pedia Not Found",
+            };
+        }
+        return {
+            status: 500,
+            message: "Unexpected server error",
+        };
+    });
+
+    return response;
+};
+
+const pediaService = {
+    getManyPedias,
+    getPediaById,
+};
+
+export default pediaService;
