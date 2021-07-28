a27aa85c5f82d2bbdd20382a93b30c08ce5d1c01
Rathijit Paul <alienpapon009@gmail.com>
2021-07-28 14:19:08 +0600

    :sparkles: Event Details Integrated

--- tourpedia/src/views/Event/index.js
+++ tourpedia/src/views/Event/index.js
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react';
 import { useLocation } from "react-router-dom";
 import Select from 'react-select';
 import {Modal, Fade, Backdrop} from '@material-ui/core';
+import { toast } from 'react-toastify';
+import ClipLoader from "react-spinners/ClipLoader";
+import LoadingOverlay from 'react-loading-overlay';
 
 import PlanMyHoliday from '../../components/PlanMyHoliday';
 import Filter from '../../components/EventFilter';
@@ -9,8 +12,10 @@ import EventLongCard from '../../components/EventLongCard';
 import LayoutWrapper from "../../layouts/LayoutWrapper";
 import "./styles.css";
 
-import countryData from "../../assets/dummyData/country.json";
-import eventData from "../../assets/dummyData/event.json";
+import exploreService from "../../services/exploreService";
+import eventService from "../../services/eventService";
+
+import fixedFilters from "../../assets/fixedFilters.json";
 
 const customStyles = {
     control: base => ({
@@ -32,6 +37,9 @@ const Event = () => {
     const location = useLocation();
     const [currentDate, setCurrentDate] = useState(new Date());
 
+    const [loading, setLoading] = useState(true);
+    const color = "#ffffff";
+
     const options = [
         { value: 'Most Popular', label: 'Most Popular' },
         { value: 'Longest Duration' , label: 'Longest Duration'},
@@ -48,7 +56,7 @@ const Event = () => {
     const [allCountryPlaceData, setAllCountryPlaceData] = useState([]);
 
     const [events, setEvents] = useState([]);
-    const [hasMore, setHasMore] = useState(true);
+    const [hasMore, setHasMore] = useState(false);
 
     const [openPlanModal, setOpenPlanModal] = useState(false);
 
@@ -56,9 +64,53 @@ const Event = () => {
         setSortOption(newValue);
     };
 
-    const handleCountryOptionChange = (newValue, actionMeta, hasData, givenData) => {
+    const getDataFromAPI = async (country, place, category) => {
+        setLoading(true);
+        const queryMatcher = {
+            durationSort: 1,
+            costSort: 1,
+            participantSort: 1,
+            date: [new Date('2000-01-01'), new Date('2100-01-01')],
+            roomSize: [1, 100],
+            accomodationQuality: [1, 100],
+            groupOption: fixedFilters.tourStyle,
+            inclusion: fixedFilters.inclusion,
+            childAllowed: false,
+            physicalRating: fixedFilters.physicalRating,
+            accomodationOption: fixedFilters.accomodationOption,
+            participantLimit: [1, 100000000],
+            duration: [1, 100000000],
+            age: [1, 1000],
+            cost: [1, 10000000000000],
+            category: category,
+            country: country,
+            place: place,
+            limit: 10000000000,
+            skip: 0,
+        };
+
+        let data = await eventService.getManyEvents(queryMatcher);
+        if (data.status >= 300) {
+            toast.error(data.message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            setLoading(false);
+            return;
+        }
+        data = data.data;
+        setEvents(data);
+        setLoading(false);
+    }
+
+    const handleCountryOptionChange = async (newValue, actionMeta, hasData, givenData) => {
         setCountryOption(newValue);
-        const placeOptions = [{ value: 'All Place', label: 'All Place' }];
+        const placeOptions = [{ value: 'All Place', label: 'All Place', id: '-1' }];
         let cpData = allCountryPlaceData;
         if (hasData) {
             cpData = givenData;
@@ -67,52 +119,112 @@ const Event = () => {
             if (data.name === newValue.value) {
                 for (const placeData of data.place) {
                     placeOptions.push({
-                        value: placeData.name,
-                        label: placeData.name
-                    })
+                        value: placeData._id.name,
+                        label: placeData._id.name,
+                        id: placeData._id._id,
+                    });
                 }
                 break;
             }
         }
         setPlace(placeOptions);
         setPlaceOption(placeOptions[0]);
+
+        await getDataFromAPI(newValue.id === '-1' ? [] : [newValue.id], [], []);
     }
 
-    const handlePlaceOptionChange = (newValue, action) => {
+    const handlePlaceOptionChange = async (newValue, action) => {
         setPlaceOption(newValue);
+        await getDataFromAPI([countryOption.id], newValue.id === '-1' ? [] : [newValue.id], []);
     }
 
     const fetchData = async () => {
-        setAllCountryPlaceData(countryData);
+        setLoading(true);
+        let data = await exploreService.getAllExplore("country");
+        if (data.status >= 300) {
+            toast.error(data.message, {
+                position: "top-right",
+                autoClose: 4000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            setLoading(false);
+            return;
+        }
+        data = data.data;
+        setAllCountryPlaceData(data);
 
-        const placeOptions = [
-            { value: 'All Country', label: 'All Country' }
-        ];
-        for (const data of countryData) {
-            placeOptions.push({
-                value: data.name,
-                label: data.name
+        const formattedData = [{
+            value: "All Country",
+            label: "All Country",
+            id: "-1"
+        }];
+        for (const country of data) {
+            formattedData.push({
+                value: country.name,
+                label: country.name,
+                id: country._id,
             });
         }
-        setCountry(placeOptions);
-        setCountryOption(placeOptions[0]);
+        setCountry(formattedData);
+        setCountryOption(formattedData[0]);
 
         setPlace([{ value: 'All Place', label: 'All Place' }]);
         setPlaceOption({ value: 'All Place', label: 'All Place' });
 
+        const queryMatcher = {
+            durationSort: 1,
+            costSort: 1,
+            participantSort: 1,
+            date: [new Date('2000-01-01'), new Date('2100-01-01')],
+            roomSize: [1, 100],
+            accomodationQuality: [1, 100],
+            groupOption: fixedFilters.tourStyle,
+            inclusion: fixedFilters.inclusion,
+            childAllowed: false,
+            physicalRating: fixedFilters.physicalRating,
+            accomodationOption: fixedFilters.accomodationOption,
+            participantLimit: [1, 100000000],
+            duration: [1, 100000000],
+            age: [1, 1000],
+            cost: [1, 10000000000000],
+            category: [],
+            country: [],
+            place: [],
+            limit: 10000000000,
+            skip: 0,
+        };
+
         if (location.state) {
             handleCountryOptionChange({
                 value: location.state.country,
-                label: location.state.country
-            }, '', true, countryData);
+                label: location.state.country,
+                id: location.state.id,
+            }, '', true, data);
             setCurrentDate(new Date(location.state.minDate));
         }
-
-        const data = []
-        for (let i = 0; i < 10; i++) {
-            data.push(eventData[0]);
+        else {
+            data = await eventService.getManyEvents(queryMatcher);
+            if (data.status >= 300) {
+                toast.error(data.message, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                setLoading(false);
+                return;
+            }
+            data = data.data;
+            setEvents(data);
+            setLoading(false);
         }
-        setEvents(data);
     }
 
     const handlePlanModal = (val) => {
@@ -120,7 +232,7 @@ const Event = () => {
     }
 
     const applyFilter = async (filters) => {
-        console.log(filters);
+        // console.log(filters);
     }
 
     const handleLoadMore = () => {
@@ -141,6 +253,13 @@ const Event = () => {
 
     return ( 
         <LayoutWrapper>
+            <LoadingOverlay
+                active={loading}
+                spinner={
+                    <ClipLoader color={color} loading={loading} size={50} />
+                }
+                className="loading-height"
+            >
             <div className="event-title">
                 Top Events
             </div>
@@ -238,6 +357,7 @@ const Event = () => {
 
             <br />
             <br />
+            </LoadingOverlay>
         </LayoutWrapper>
      );
 }
